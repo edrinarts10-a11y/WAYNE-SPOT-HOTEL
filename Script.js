19f95d836af577fa85733e7a669ff5c7681bf232
edrinarts10-a11y <edrinarts10@gmail.com>
2025-11-24 14:28:07 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+/* slide Bar */
+    function openNav() {
+    document.getElementById("mysidebar").style.left = "0";
+    document.getElementById("overlay").style.display = "block";
+  }
+ function closeNav() {
+   document.getElementById("mysidebar").style.left = "-250px"
+   document.getElementById("overlay").style.display = "none"
